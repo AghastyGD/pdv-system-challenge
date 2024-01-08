32a9a58e7aeaa9a1bd5fbd5075455d5c52b471aa
AghastyGD <augustodomingosvasco@gmail.com>
2024-01-08 07:07:20 -0200

    remove user list

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import CategoriaApiView, UsuarioApiView, UsuarioList, UsuarioLogin
+from .views import CategoriaApiView, UsuarioApiView, UsuarioLogin
 
 urlpatterns = [
     path('categoria', CategoriaApiView.as_view()),
     path('usuario', UsuarioApiView.as_view()),
-    path('user-list', UsuarioList.as_view()),
     path('login', UsuarioLogin.as_view()),
 
 ]
